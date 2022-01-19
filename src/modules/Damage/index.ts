b3f7f420ead79ae60653f22c5236bf7bffcce13d
YueAo7 <leaoqi@outlook.com>
2022-01-19 17:53:31 +0800

    ✨ feat(伤害基类): 添加新属性
    添加了普通增伤、额外伤害值、倍率增幅三个属性以及类型结构

--- src/modules/Damage/index.ts
+++ src/modules/Damage/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-01-17 18:23:27
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-18 16:46:51
+ * @LastEditTime: 2022-01-19 16:06:30
  * @FilePath: \noelle-core-v2\src\modules\Damage\index.ts
  */
 
@@ -11,6 +11,12 @@ import { ControlModel } from "../Control";
 import { SkillModel } from "../Skill";
 
 export namespace DamageModel {
+    type DMGModifyType = "倍率增幅"|"额外伤害"|"普通增伤"
+    type DMGModifyHistroy={
+        type:DMGModifyType,
+        val:number,
+        label:string
+    }
     type ResultProp = {
         avg: number,
         max: number,
@@ -44,7 +50,19 @@ export namespace DamageModel {
         DMGType: DamageType | undefined = undefined
         /**伤害标签 */
         tag: string
-        /** */
+        /**增伤 */
+        DMGRate:DMGModifyHistroy[] =[]
+        /**额外伤害 */
+        DMGExtra:DMGModifyHistroy[] =[]
+        /**倍率提升 */
+        DMGRateRate:DMGModifyHistroy[] =[]
+
+        /**
+         * 
+         * @param from 伤害来源
+         * @param skill 伤害技能
+         * @param to 伤害目标
+         */
         constructor(from: ControlModel.Control, skill: SkillModel.Skill<Damage | CharacterModel.Character>, to: ControlModel.Control) {
             this.ID = skill.ID
             this.from = from
