01fa3c7ccbc9622b1be0c5e5f621529cb1ec8c6c
YueAo7 <leaoqi@outlook.com>
2022-01-20 17:32:18 +0800

    🦄 refactor(核心导出): 修改了结构

--- src/Core.ts
+++ src/Core.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-01-16 09:18:52
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-19 16:26:55
+ * @LastEditTime: 2022-01-20 16:50:30
  * @FilePath: \noelle-core-v2\src\Core.ts
  */
 import { ArtifactModel } from "./modules/Artifact"
@@ -9,58 +9,77 @@ import { Atom } from "./modules/Atom"
 import { BuffModel } from "./modules/Buff"
 import { CharacterModel } from "./modules/Character"
 import { ControlModel } from "./modules/Control"
+import { DamageModel } from "./modules/Damage"
 import { SkillModel } from "./modules/Skill"
 import { WeaponModel } from "./modules/Weapon"
-export const Core = ControlModel
-export namespace Noelle{
-    export namespace Type {
-        /**技能数据类型 */
-        export type Skill = SkillModel.SkillData<ControlModel.Control>
-        /**角色数据类型 */
-        export type Character = ControlModel.CharacterRoleData
-        /**武器数据类型 */
-        export type Weapon = ControlModel.WeaponData
-        /**圣遗物套装数据类型 */
-        export type ArtifactSet = ControlModel.ArtifactData
-        
-    }
-    export namespace Class{
-        /**BUFF类 */
-        export class Buff extends BuffModel.Buff{}
-        /**原子属性 */
-        export class Prop extends Atom.Prop{}
-    }
-    export namespace Tools{
-        /**
-         * 生成延时配置对象
-         * @param start 起始帧
-         * @param time 持续时间
-         * @returns 
-         */
-        export function getFrame(start:number, time:number){
-            const end = start +Math.floor(time*60)
-            return {
-                start,
-                end
-            } 
+
+export namespace Types {
+    export type Character = ControlModel.CharacterRoleData
+    export type Skill = SkillModel.SkillData<ControlModel.Control>
+    export type Weapon = ControlModel.WeaponData
+    export type ArtifactSet = ControlModel.ArtifactData
+    export type SkillRate = SkillModel.DMGRateDetail
+}
+export const Classes = {
+    Prop: Atom.Prop,
+    Buff: BuffModel.Buff,
+    Damage: DamageModel.Damage
+}
+export const Tools = {
+    getFrame(start: number, time: number) {
+        const end = start + Math.floor(time * 60)
+        return {
+            start,
+            end
         }
+    },
+    DMGRate(data:Types.SkillRate,lv:number): DamageModel.Rate[] {
+        const result:DamageModel.Rate[] =[]
+        data.row.map(item=>{
+            result.push({
+                prop:item.use,
+                rate:item.rate[lv-1]
+            })
+        })
+        return result
     }
 }
-export type CharacterData = Noelle.Type.Character
-export type SkillData = Noelle.Type.Skill
-export type WeaponData = Noelle.Type.Weapon
-export type ArtifactSetData = Noelle.Type.ArtifactSet
-export class Prop extends Noelle.Class.Prop{}
-export class Buff extends  Noelle.Class.Buff{}
-export const getFrame = Noelle.Tools.getFrame
+export namespace Noelle {
+    export namespace Type {
+        export type Character = ControlModel.CharacterRoleData
+        export type Skill = SkillModel.SkillData<ControlModel.Control>
+        export type Weapon = ControlModel.WeaponData
+        export type ArtifactSet = ControlModel.ArtifactData
+    }
+    export const Class = Classes
+    export const Tool = Tools
+    export const load = ControlModel.Control.loadData
+}
+export default Noelle
+import {诺艾尔} from "./default/诺艾尔"
+import {木桩} from "./default/木桩"
+Noelle.load([木桩,诺艾尔])
+const x = new ControlModel.Control("诺艾尔")
+const mod = new ControlModel.Control("木桩")
+x.Q(x, 1)
 
-
-import {诺艾尔}  from "./testDB"
-ControlModel.Control.loadData(诺艾尔)
-const x= new ControlModel.Control("诺艾尔")
-x.Q(x,1)
-x.Q(x,2)
-x.Q(x,903)
-console.log(x.Last.atk);
+x.setArtifact({
+    type: "时之沙",
+    name: "TEST",
+    main: {
+        elementDMGGeo: 0.466
+    },
+    set: "TEST",
+    other: {
+        atkRate: 0.466,
+        defRate: 1230
+    }
+})
+x.Q(x, 11)
+x.A(mod,12)
+x.A(mod,12)
+x.A(mod,12)
+x.A(mod,12)
+console.log(x.Last.atk.content);
 
 
