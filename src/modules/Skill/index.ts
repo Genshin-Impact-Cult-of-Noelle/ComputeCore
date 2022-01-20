a993d940e8de6adb37834d8ccdacd4324bf90817
YueAo7 <leaoqi@outlook.com>
2022-01-20 17:24:10 +0800

    ✨ feat(技能类): 倍率结构

--- src/modules/Skill/index.ts
+++ src/modules/Skill/index.ts
@@ -1,38 +1,49 @@
 /*
  * @Date: 2022-01-17 18:24:58
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-19 16:34:08
+ * @LastEditTime: 2022-01-20 14:52:20
  * @FilePath: \noelle-core-v2\src\modules\Skill\index.ts
  */
 import { Molecule } from "../Molecule"
 import { CharacterModel } from "../Character"
 import { BuffModel } from "../Buff"
 import { DamageModel } from "../Damage"
+import { Atom } from "../Atom"
 export namespace SkillModel {
+    export type DMGRateDetail = {
+        row: {
+            use: keyof Atom.ObjectProps | undefined,
+            rate: number[]
+        }[],
+        delay: { must: number, last: number }
+    }
     type SkillOutput = {
         delay?: {
             must: number,
             last: number
         }
-        result?: DamageModel.Damage
+        result?: DamageModel.Damage<unknown>
     }
     type SkillFnc<FncTarget> = {
         (from: FncTarget, to: FncTarget, frameTime: number, lv?: number): SkillOutput
     }
+    interface ControlFnc<Target>{
+        (from: Target, target: Target, frameTime: number, lv: number):void|SkillOutput
+    }
     export type SkillData<FncTarget> = {
         /**技能名 */
         name: string,
-        /**技能函数 */
-        fnc: SkillFnc<FncTarget>
+        /**技能初始函数 */
+        init(skill: Skill<FncTarget>): void
     }
-    export class Skill<Target> implements SkillData<Target> {
+    export class Skill<Target>{
         ID: symbol = Symbol()
         name: string = "未知技能"
-        fnc: SkillFnc<Target> = (f, t, l?) => { return {} }
+        fnc: SkillFnc<Target> = (f, t,time, l?) => { return {} }
         constructor(data?: SkillData<Target>) {
             if (data) {
                 this.name = data.name
-                this.fnc = data.fnc
+                data.init(this)
             }
         }
     }
@@ -45,27 +56,34 @@ export namespace SkillModel {
         elementBurst: SkillData<Target>
     }
     /**角色技能模组 */
-    export class CharacterSkillControl<Target> implements CharacterSkillData<Target>{
-        normalAttack: Skill<Target> = new Skill()
-        elementSkill: Skill<Target> = new Skill()
-        elementBurst: Skill<Target> = new Skill()
+    export class CharacterSkillControl<Target>{
+        private _normalAttack: Skill<Target> = new Skill()
+        private _elementSkill: Skill<Target> = new Skill()
+        private _elementBurst: Skill<Target> = new Skill()
         constructor(data?: CharacterSkillData<Target>) {
             if (data) {
                 const { normalAttack, elementSkill, elementBurst } = data
-                this.normalAttack = new Skill<Target>(normalAttack)
-                this.elementSkill = new Skill<Target>(elementSkill)
-                this.elementBurst = new Skill<Target>(elementBurst)
+                this._normalAttack = new Skill<Target>(normalAttack)
+                this._elementSkill = new Skill<Target>(elementSkill)
+                this._elementBurst = new Skill<Target>(elementBurst)
             }
         }
-        Atk() { }
-        AtkCharged() { }
-        AtkJumpLow() { }
-        AtkJumpHigh() { }
-        Skill() { }
-        SkillLong() { }
-        Burst(from:Target, target:Target,frameTime:number,lv:number) {
-            return this.elementBurst.fnc(from,target,frameTime,lv)
-         }
+        Atk:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+            return this._normalAttack.fnc(from,target,frameTime,lv)
+        }
+        AtkCharged:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+        }
+        AtkJumpLow:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+        }
+        AtkJumpHigh:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+        }
+        Skill:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+        }
+        SkillLong:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+        }
+        Burst:ControlFnc<Target>=(from, target, frameTime, lv)=> {
+            return this._elementBurst.fnc(from, target, frameTime, lv)
+        }
     }
     type EquipSkillObject<T> = {
         [key: symbol]: Skill<T>
@@ -85,7 +103,7 @@ export namespace SkillModel {
             const SkillResult = Object.getOwnPropertySymbols(this.skillList).map(item => {
                 return this.skillList[item].fnc(target, target, 0)
             })
-            const result: BuffModel.Buff[] = []
+            const result: BuffModel.Buff<T>[] = []
             SkillResult.map(item => {
                 if (item.result instanceof BuffModel.Buff) {
                     result.push(item.result)
