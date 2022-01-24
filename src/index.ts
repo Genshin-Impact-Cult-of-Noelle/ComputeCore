5b613c1ec360654de6539da4129d9e4feec0e02a
YueAo7 <leaoqi@outlook.com>
2022-01-24 19:26:46 +0800

    ✨ feat(核心): 暴露属性
    提供控制器创建属性，提供流程创建属性

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-01-16 09:18:52
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-24 15:18:45
+ * @LastEditTime: 2022-01-24 16:58:29
  * @FilePath: \noelle-core-v2\src\index.ts
  */
 import { Atom } from "./modules/Atom"
@@ -9,8 +9,9 @@ import { BuffModel } from "./modules/Buff"
 import { ControlModel } from "./modules/Control"
 import { DamageModel } from "./modules/Damage"
 import { SkillModel } from "./modules/Skill"
+import {ActionModel } from  "./modules/Action"
 import * as Enum from "./enum"
-export namespace Types {
+export namespace Types {    
     export type Character = ControlModel.CharacterRoleData
     export type Skill = SkillModel.SkillData<ControlModel.Control>
     export type Weapon = ControlModel.WeaponData
@@ -18,11 +19,20 @@ export namespace Types {
     export type SkillRate = SkillModel.DMGRateDetail
 }
 export const Classes = {
+    /**基础属性类 */
     Prop: Atom.Prop,
+    /**Buff类 */
     Buff: BuffModel.Buff,
+    /**伤害类 */
     Damage: DamageModel.Damage
 }
 export const Tools = {
+    /**
+     * 取得帧间隔对象
+     * @param start 开始帧位
+     * @param time 耗时
+     * @returns 帧间隔对象
+     */
     getFrame(start: number, time: number) {
         const end = start + Math.floor(time * 60)
         return {
@@ -30,6 +40,12 @@ export const Tools = {
             end
         }
     },
+    /**
+     * 通过技能等级命中技能倍率中的数据
+     * @param data 倍率数据
+     * @param lv 技能等级
+     * @returns 
+     */
     DMGRate(data:Types.SkillRate,lv:number): DamageModel.Rate[] {
         const result:DamageModel.Rate[] =[]
         data.row.map(item=>{
@@ -42,16 +58,30 @@ export const Tools = {
     }
 }
 export namespace Noelle {
+    /**类型结构 */
     export namespace Type {
+        /**角色数据结构 */
         export type Character = Types.Character
+        /**技能数据结构 */
         export type Skill = Types.Skill
+        /**武器数据结构 */
         export type Weapon = Types.Weapon
+        /**圣遗物套装数据结构 */
         export type ArtifactSet = Types.ArtifactSet
+        /**技能倍率数据结构 */
         export type SkillRate = Types.SkillRate
     }
+    /**类 */
     export const Class = Classes
+    /**工具区 */
     export const Tool = Tools
+    /**挂载数据 */
     export const load = ControlModel.Control.loadData
+    /**常量 */
     export const ConstData = Enum
+    /**控制器类 */
+    export const Control = ControlModel.Control
+    /**流程 */
+    export const Action = ActionModel.Action
 }
 export default Noelle
