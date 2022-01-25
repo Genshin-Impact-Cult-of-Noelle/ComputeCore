0359d201b41799b023df26f6e1eda9e514a51933
YueAo7 <leaoqi@outlook.com>
2022-01-25 17:57:42 +0800

    ✨ feat(流程控制器): 完善流程
    编写中

--- src/modules/Action/index.ts
+++ src/modules/Action/index.ts
@@ -1,41 +1,86 @@
 /*
  * @Date: 2022-01-19 14:03:32
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-19 17:40:12
+ * @LastEditTime: 2022-01-25 13:59:14
  * @FilePath: \noelle-core-v2\src\modules\Action\index.ts
  */
 import { ControlModel } from "../Control"
 
 export namespace ActionModel {
     type ActionCMD = "普通攻击" | "元素战技" | "元素爆发" | "闪避" | "切换" | "等待"
+    type WaitModel = "must" | "last"
     type ActionItem = {
         CMD: ActionCMD,
-        data: symbol
+        character?: symbol
+        delay?: number
     }
+    const noneDelay = { must: 0, last: 0 }
     export class Action {
+        private LastDo: ActionCMD = "等待"
         private frame: number = 0
+        private delayCache: { [key in WaitModel]: number } = { ...noneDelay }
         private toDo: ActionItem[] = []
-        do(cmd: ActionCMD, control?: ControlModel.Control) {
+        private control?:ControlModel.Control
+        do(cmd: ActionCMD, data?: ControlModel.Control | number) {
             const action: ActionItem = {
                 CMD: cmd,
-                data: Symbol()
             }
-            if (control) {
-                action.data = control.character.core.ID
+            if (data instanceof ControlModel.Control && cmd === "切换") {
+                action.character = data.character.core.ID
+            } else if (typeof data === "number" && cmd === "等待") {
+                action.delay = data
             }
             this.toDo.push(action)
+            return this
         }
         private check(target: ControlModel.Control) {
-            if (this.toDo.filter(item => item.CMD === "切换").map(item => {
-                return target.team.inTeam(item.data)
-            }).some(item => !item)) return false
+            if (this.toDo
+                .filter(item => item.CMD === "切换")
+                .map(item => {
+                    return target.team.inTeam(item.character ?? Symbol())
+                })
+                .some(item => !item)) return false
             return true
         }
-        play(target: ControlModel.Control) {
-            if (this.check(target)) {
+        private waitTo(model: WaitModel) {
+            const to = this.frame + this.delayCache[model]
+            while (to > this.frame) {
+                this.frame++
+                this.control?.refreshAll(this.frame)
+            }
+        }
+        play(target: ControlModel.Control, toMod: ControlModel.Control) {
+            if (!this.check(target)) {
                 return false
             } else {
+                this.control=target
+                for (const iterator of this.toDo) {
+                    this.frame++
+                    switch (iterator.CMD) {
+                        case "普通攻击":
+                            this.waitTo("last")     
+                          this.delayCache=  this.control.A(toMod,this.frame)?.delay??{...noneDelay}                       
+                            break;
+                        case "元素爆发":
+                            if(["普通攻击"].includes(this.LastDo)) {
+                                this.waitTo("must")
+                            }else{
+                                this.waitTo("last")
+                            }     
+                            this.delayCache=  this.control.E(toMod,this.frame)?.delay??{...noneDelay}                   
 
+                            break;
+                        case "元素战技":
+                            this.control.Q(toMod,this.frame)  
+                            this.delayCache=  this.control.Q(toMod,this.frame)?.delay??{...noneDelay} 
+                            break;
+                        case "等待":
+
+                        default:
+                            break;
+                    }
+                }
+                return true
             }
         }
         clean() {
