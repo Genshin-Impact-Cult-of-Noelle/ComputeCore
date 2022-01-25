076e7e75a5c7e1151930943ebd46be3c2526e093
YueAo7 <leaoqi@outlook.com>
2022-01-25 17:56:46 +0800

    🎈 perf(工具类): 优化
    现在工具类能正常的使用了

--- src/common/function.ts
+++ src/common/function.ts
@@ -1,38 +1,39 @@
 /*
  * @Date: 2022-01-20 16:09:57
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-20 16:48:30
+ * @LastEditTime: 2022-01-25 16:01:20
  * @FilePath: \noelle-core-v2\src\common\function.ts
  */
-export function ObjectThin(data: any):Object|boolean {
-    if (data instanceof Object) {
+export function ObjectThin(data: unknown): Object | boolean {
+    if (data instanceof Array) {
+        return data.filter(item=>ObjectThin(item))
+    } else if (typeof data === "object" && data) {
         const keys = Object.keys(data)
         if (!keys.length) {
             return false
         }
+        const temp={}
         keys.map(item => {
-            if(item=="levelData"){
-                delete data[item]
-            }else            if(!ObjectThin(data[item])){
-                delete data[item]
+            if (item == "DMGHistroy" || item == "levelData") {
+            } else  {
+                const res = ObjectThin(data[item])
+                if (res){
+                    temp[item] = res
+                }
             }
         })
-        if(Object.keys(data).length){
-            return data
-        }else{
+        if (Object.keys(temp).length) {
+            return temp
+        } else {
             return false
         }
-        
-
-    } else if (data instanceof Array) {
-        return data.some(item=>ObjectThin(item))
-    }else if(typeof data ==="function"){
+    } else if (typeof data === "function") {
         return false
-    }else if(typeof data ==="number"){
-        return !!data
-    }else if(typeof data ==="string"){
-        return !!data
-    }else {
+    } else if (typeof data === "number") {
+        return data ?? false
+    } else if (typeof data === "string") {
+        return data ?? false
+    } else {
         return true
     }
 
--- src/common/typeTool.ts
+++ src/common/typeTool.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-01-16 16:01:58
  * @LastEditors: YueAo7
- * @LastEditTime: 2022-01-18 16:29:57
+ * @LastEditTime: 2022-01-25 15:37:09
  * @FilePath: \noelle-core-v2\src\common\typeTool.ts
  */
 
@@ -9,7 +9,7 @@
 
 
 export namespace Common {
-    type BaseType<T = any> = T[]
+    type BaseType = Array<unknown>
     export type Keys<T> = keyof T
     export type Shift<T extends BaseType> =
         T extends [infer A, ...infer B]
